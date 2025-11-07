25b36bd7110d55e6e968aeff266068c534134edd
Soumik Mitra <soumikdeb2@gmail.com>
2025-11-07 11:50:07 -0500

    Unused package import was used. Commented it out. Print is not used in the production, so included a comment to ignore that

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
                       const SizedBox(height: 20),
                       ElevatedButton(
                         onPressed: () {
+                          //ignore: avoid_print
                           print('Click');
                         },
                         child: const Text('A button'),
